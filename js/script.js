57fc4f28359b7a4b05facc0871eaf55787047fae
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-03-07 12:56:20 +0100

    calculateResult function added

--- js/script.js
+++ js/script.js
@@ -1,53 +1,50 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
-let currencyElement = document.querySelector(".js-currency");
-let resetButton = document.querySelector(".js-reset");
+{
+    let formElement = document.querySelector(".js-form");
+    let amountElement = document.querySelector(".js-amount");
+    let resultElement = document.querySelector(".js-result");
+    let currencyElement = document.querySelector(".js-currency");
+    let resetButton = document.querySelector(".js-reset");
 
 
 
+    const calculateResult = (amount, currency) => {
 
+        const rateEUR = 4.3330;
+        const rateUSD = 4.0030;
+        const rateCHF = 4.5369;
+        const rateGBP = 5.0606;
 
-const rateEUR = 4.3330;
-const rateUSD = 4.0030;
-const rateCHF = 4.5369;
-const rateGBP = 5.0606;
+        switch (currency) {
+            case "EUR":
+                return amount / rateEUR;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+            case "USD":
+                return amount / rateUSD;
 
+            case "CHF":
+                return amount / rateCHF;
 
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
+            case "GBP":
+                return amount / rateGBP;
+        };
 
-    let result;
+    }
 
-    switch (currency) {
-        case "EUR":
-            result = amount / rateEUR;
-            break;
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-        case "USD":
-            result = amount / rateUSD;
-            break;
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
 
-        case "CHF":
-            result = amount / rateCHF;
-            break;
+        let result = calculateResult(amount, currency);
 
-        case "GBP":
-            result = amount / rateGBP;
-            break;
-    };
+        resultElement.innerHTML = `You will receive <strong class="outputAmount">${result.toFixed(2)} ${currency}</strong>`;
+    });
 
-
-    resultElement.innerHTML = `You will receive <strong class="outputAmount">${result.toFixed(2)} ${currency}</strong>`;
-});
-
-resetButton.addEventListener("click", () => {
-    resultElement.innerHTML = ``;
-})
-
+    resetButton.addEventListener("click", () => {
+        resultElement.innerHTML = ``;
+    })
+}
 
 
 
