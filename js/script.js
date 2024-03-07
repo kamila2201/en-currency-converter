3cf1f7812ce0ba302bd6063df6dc59592d87d428
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-03-07 13:17:05 +0100

    added updateResultText function and replaced 'let' with 'const'

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 {
-    let formElement = document.querySelector(".js-form");
-    let amountElement = document.querySelector(".js-amount");
-    let resultElement = document.querySelector(".js-result");
-    let currencyElement = document.querySelector(".js-currency");
-    let resetButton = document.querySelector(".js-reset");
-
-
-
     const calculateResult = (amount, currency) => {
 
         const rateEUR = 4.3330;
@@ -30,20 +22,36 @@
 
     }
 
+    const updateResultText = (result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `You will receive <strong class="outputAmount">${result.toFixed(2)} ${currency}</strong>`;
+    }
+
+    const formElement = document.querySelector(".js-form");
+
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
 
         const amount = +amountElement.value;
         const currency = currencyElement.value;
 
-        let result = calculateResult(amount, currency);
+        const result = calculateResult(amount, currency);
 
-        resultElement.innerHTML = `You will receive <strong class="outputAmount">${result.toFixed(2)} ${currency}</strong>`;
+        updateResultText(result, currency);
     });
 
-    resetButton.addEventListener("click", () => {
-        resultElement.innerHTML = ``;
-    })
+    
+
+    // const deleteResult = () => {
+    //     resultElement.innerHTML = ``;
+    // }
+
+    // let resetButton = document.querySelector(".js-reset");
+
+    // resetButton.addEventListener("click", deleteResult());
 }
 
 
